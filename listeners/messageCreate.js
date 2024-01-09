24872787206cfa896531a3e2e55740e77a9c4854
ItzYourBread <72562619+ItzYourBread@users.noreply.github.com>
2024-01-09 17:21:02 +0000

    Fixed

--- listeners/messageCreate.js
+++ listeners/messageCreate.js
@@ -3,15 +3,15 @@ import 'dotenv/config';
 
 export async function messageCreate(client) {
   client.on('messageCreate', async (msg) => {
-    if (msg.channel.id === '1111034643664355488') {
+    if (msg.channel.id === '1194186144406061156') {
       if (msg.content) {
         await msg.delete(); // Delete the text message
       }
 
       if (msg.attachments.length > 0) {
         // React to the message with smash and pass emojis
-        await msg.addReaction('smash:1116683253223534695'); // Smash reaction
-        await msg.addReaction('pass:1116683299352490064'); // Pass reaction
+        await msg.addReaction('wn_smash:1194234030552514651'); // Smash reaction
+        await msg.addReaction('wn_pass:1194234138534875137'); // Pass reaction
       }
     }
   });
