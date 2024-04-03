00516cd75260fe5ad93f4cb6e7cd5b48d71a7af0
Arif <72562619+ItzYourBread@users.noreply.github.com>
2024-04-03 13:38:40 +0000

    Fixed

--- system/anti-links.js
+++ system/anti-links.js
@@ -6,37 +6,26 @@ const WhiteListRoles =
 		"1177650373704564877" // verified role
 	];
 
-const linkCounter = new Map();
-
 export function anti_links(client) {
 	client.on("messageCreate", async (msg) => {
-		const links =
-			/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
+		const links = /(https?:\/\/[^\s]+)/g; // Regex to match URLs
 		if (links.test(msg.content)) {
 			if (msg.member && msg.member.roles.some(role => WhiteListRoles.includes(role))) {
 				return;
 			}
 
-			const userID = msg.member.id;
-			linkCounter.set(userID, (linkCounter.get(userID) || 0) + 1);
-
 			const guild = client.guilds.get("1176811262202626118");
 			const member = guild.members.get(msg.member.id);
 
-			if (linkCounter.get(userID) >= 1) {
-				if (msg.member) {
-					// ban the user instead muting it.
-					await member.ban({
-						deleteMessageDays: 7,
-						reason: "Sending Unwanted links",
-					});
+			if (member) {
+				// Check if the user has sent only one link
+				if (msg.content.match(links).length === 1) {
+					// Ban the user with 7-day timeout
+					await member.ban(7, "Sending Unwanted links");
+					client.deleteMessage(msg.channel.id, msg.id);
+					return;
 				}
-				linkCounter.delete(userID);
-				client.deleteMessage(msg.channel.id, msg.id);
-				return;
 			}
-
-			client.deleteMessage(msg.channel.id, msg.id);
 		}
 	});
 }
